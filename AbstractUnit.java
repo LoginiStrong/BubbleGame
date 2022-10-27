5228d92ffcf9cf183ae62890394a5c3d78a2ce94
vpnoah <noahsd1@aol.com>
2022-10-27 10:04:57 -0700

    more class stubs

---
+++ AbstractUnit.java
@@ -0,0 +1,7 @@
+public abstract class AbstractUnit {
+    
+    //needs a abstract "command" method
+    //should have x and y positions?
+    //pretty sure that this will feed into the bubble class
+
+}
