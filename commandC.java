916dc664dc3b45407855d3aaa6306838d0744aa6
vpnoah <noahsd1@aol.com>
2022-11-03 10:00:24 -0700

    did commands XD

--- commandC.java
+++ commandC.java
@@ -1,23 +1,17 @@
 public class commandC implements command{
 
-    Player p;
-    int mouseX;
-    int mouseY;
 
-    public commandC(Player p, int mouseX, int mouseY){
+    public commandC(){
 
-        this.p = p;
-        this.mouseX = mouseX;
-        this.mouseY = mouseY;
 
     }
 
     public void execute(Player p1, int mouseX, int mouseY){
         System.out.println("this should print the c command");
 
-        if(p.selected){
+        if(p1.selected){
 
-            p.setVelocity(mouseX, mouseY); //do something about graphics translation
+            p1.setVelocity(mouseX, mouseY); //do something about graphics translation
 
          }
 
